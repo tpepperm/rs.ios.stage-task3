567e6cd48f2ec185acb8ed5e483f70081b9f5934
tpepperm <tkarpukova@gmail.com>
2021-05-24 23:41:19 +0300

    feat: task 3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,47 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    if (monthNumber < 1 || monthNumber > 12) return nil;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    NSString *month = [[formatter monthSymbols] objectAtIndex:(monthNumber - 1)];
+
+    return month;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+
+    NSDate *newDate = [formatter dateFromString:date];
+    if (newDate == nil) {
+        return 0;
+    }
+
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *components = [calendar components:NSCalendarUnitDay fromDate:newDate];
+    return [components day];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *weekday = [[NSDateFormatter alloc] init];
+    [weekday setLocale:[NSLocale localeWithLocaleIdentifier:@"ru_RU"]];
+    [weekday setDateFormat: @"EE"];
+    return [weekday stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSDate *startDate = nil;
+    NSTimeInterval interval = 0.0;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    [calendar rangeOfUnit:NSCalendarUnitWeekOfYear startDate:&startDate interval:&interval forDate:[NSDate date]];
+    NSDate *endDate = [calendar dateByAddingUnit:NSCalendarUnitSecond value:(NSInteger)interval toDate:startDate options:NSCalendarMatchNextTime];
+    return [date compare:startDate] == NSOrderedDescending && [date compare:endDate] == NSOrderedAscending;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,24 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *first = [NSMutableString stringWithString:string1];
+    NSMutableString *second = [NSMutableString stringWithString:string2];
+    NSMutableString *result = [[NSMutableString alloc] init];
+
+    NSRange range = NSMakeRange(0, 1);
+    while (first.length > 0 && second.length > 0) {
+        NSComparisonResult comparison = [first compare:second];
+        if (comparison == NSOrderedAscending) {
+            [result appendString:[first substringWithRange:range]];
+            [first deleteCharactersInRange:range];
+        } else {
+            [result appendString:[second substringWithRange:range]];
+            second = [second substringFromIndex:1];
+        }
+    }
+    [result appendString:first];
+    [result appendString:second];
+    return  [NSString stringWithString:result];
 }
 
 @end
